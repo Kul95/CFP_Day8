e41f61f61554b7b33b2ed3162d08db8d611f2075
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-16 00:08:45 +0530

    Added:UC4

--- src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
+++ src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
@@ -1,24 +1,19 @@
 package com.Practice.EmployeeWageProblem;
 
 public class EmployeeWageProblem {
+    public  static  final  int IS_PART_TIME=1;
+    public  static  final  int IS_FULL_TIME=2;
+    public  static  final int EMP_RATE_PER_HOUR=30;
     public static void main(String[] args) {
-        //Constant variables
-        int IS_PART_TIME=1;
-        int IS_FULL_TIME = 2;
-        int empRate_Per_Hour=30;
-        //Variables
-        int empHours=0;
-        int empWage=0;
-        //Condition
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_PART_TIME)
-            empHours=4;
-            else if(empCheck==IS_FULL_TIME)
-                empHours=8;
-        else
-            empHours=0;
-            empWage=empHours*empRate_Per_Hour;
-            System.out.println("EmployeeWage: "+empWage);
-
+int empHours=0;
+int empWage=0;
+int empCheck=(int)Math.floor(Math.random()*10)%3;
+switch (empCheck){
+    case IS_PART_TIME:empHours=4;break;
+    case IS_FULL_TIME:empHours=8;break;
+    default:empHours=0;
+}
+empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: "+empWage);
     }
 }
