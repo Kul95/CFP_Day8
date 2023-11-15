d11b26cd05a06af6ff879f099f119ecf114e8685
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-15 23:16:42 +0530

    Added: Welcome to EmoloyeeWage

---
+++ src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
@@ -0,0 +1,7 @@
+package com.Practice.EmployeeWageProblem;
+
+public class EmployeeWageProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage Problem.");
+    }
+}
