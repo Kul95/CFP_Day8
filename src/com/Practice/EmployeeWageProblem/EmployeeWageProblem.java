b1c7662a4cf1ba3b5108064d239c78370ec57ae8
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-15 23:56:19 +0530

    Added: UC3

--- src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
+++ src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
@@ -2,18 +2,23 @@ package com.Practice.EmployeeWageProblem;
 
 public class EmployeeWageProblem {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        //Constant variables
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME = 2;
         int empRate_Per_Hour=30;
+        //Variables
         int empHours=0;
         int empWage=0;
+        //Condition
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            empHours=8;
-
-        } else {
+        if (empCheck == IS_PART_TIME)
+            empHours=4;
+            else if(empCheck==IS_FULL_TIME)
+                empHours=8;
+        else
             empHours=0;
             empWage=empHours*empRate_Per_Hour;
             System.out.println("EmployeeWage: "+empWage);
-        }
+
     }
 }
