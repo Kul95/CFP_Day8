3c89d39eebae0b0d39197433508e3c362d12a77d
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-15 23:44:57 +0530

    Added: UC2 problem

--- src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
+++ src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
@@ -3,11 +3,17 @@ package com.Practice.EmployeeWageProblem;
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int empRate_Per_Hour=30;
+        int empHours=0;
+        int empWage=0;
         double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is present");
+            empHours=8;
+
         } else {
-            System.out.println("Employee is not present");
+            empHours=0;
+            empWage=empHours*empRate_Per_Hour;
+            System.out.println("EmployeeWage: "+empWage);
         }
     }
 }
