f90c256c8ed55fe83b7d5201eb8d1f407d03bd29
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-15 23:22:50 +0530

    Added: UC1 Problem

--- src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
+++ src/com/Practice/EmployeeWageProblem/EmployeeWageProblem.java
@@ -2,6 +2,12 @@ package com.Practice.EmployeeWageProblem;
 
 public class EmployeeWageProblem {
     public static void main(String[] args) {
-        System.out.println("Welcome to EmployeeWage Problem.");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is not present");
+        }
     }
 }
